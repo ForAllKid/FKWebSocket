7e1607a8d9fd1a531cb3b3bdf594055c9a402cc7
ForKid <910784384@qq.com>
2017-07-05 13:57:54 +0800

    add files

---
+++ FKWebSocket/FKWebSocket/FKIMClient.m
@@ -0,0 +1,13 @@
+//
+//  FKIMClient.m
+//  FKWebSocket
+//
+//  Created by 周宏辉 on 2017/7/5.
+//  Copyright © 2017年 ForKid. All rights reserved.
+//
+
+#import "FKIMClient.h"
+
+@implementation FKIMClient
+
+@end
